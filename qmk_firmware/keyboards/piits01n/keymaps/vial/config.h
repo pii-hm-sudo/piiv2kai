879adbcd522c68d86f59b01b386dfdceebe86637
pii-hm-sudo <pii-hm@mh.point.ne.jp>
2025-09-04 13:38:42 +0900

    Add files via upload

---
+++ qmk_firmware/keyboards/piits01n/keymaps/vial/config.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x0a, 0x3f, 0x37, 0x8d, 0xc1, 0xe7, 0xca, 0x7d}
+#define VIAL_TAP_DANCE_ENTRIES 8
+#define VIAL_COMBO_ENTRIES 8
+#define PICO_FLASH_SIZE_BYTES (1 * 1024 * 1024)
